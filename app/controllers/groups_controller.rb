24e8ab3ac65cac1efb4600be37a74730bf33b71c
dai-guu <daiguu2021@gmail.com>
2022-01-15 07:33:06 +0000

    [Update]Group機能

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,9 +1,70 @@
 class GroupsController < ApplicationController
+
+
   def index
     @user = current_user
+    @groups = Group.all
   end
-  
-  def user_params
-    params.require(:user).permit(:name, :introduction, :profile_image)
+
+  def show
+    # @list = List.new
+    @group = Group.find(params[:id])
   end
+
+  def join
+    @group = Group.find(params[:group_id])
+    @group.users << current_user
+    redirect_to  groups_path
+  end
+
+
+  def new
+    @group = Group.new
+  end
+
+  def create
+    @group = Group.new(group_params)
+    @group.owner_id = current_user.id
+    @group.users << current_user  #current_userを追加
+    if @group.save
+      redirect_to groups_path
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+    @group = Group.find(params[:id])
+  end
+
+  def update
+    @group = Group.find(params[:id])
+    if @group.update(group_params)
+
+      redirect_to groups_path
+    else
+      render 'edit'
+    end
+  end
+
+ 
+  def destroy                          # 退会機能
+    @group = Group.find(params[:id])
+    @group.users.delete(current_user)
+      redirect_to groups_path
+  end
+
+  private
+
+  def group_params
+    params.require(:group).permit(:name, :introduction, :image)
+  end
+
+  def ensure_correct_user
+    @group = Group.find(params[:id])
+    unless @group.owner_id == current_user.id
+      redirect_to groups_path
+    end
+  end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,18 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-         
+
   attachment :profile_image
+
+  has_many :group_users   #中間テーブル！
+  has_many :groups, through: :group_users,dependent: :destroy
+  
+  # has_many :owned_groups, class_name: "Group"
+  # has_many :favorites, dependent: :destroy
+  # has_many :book_comments, dependent: :destroy
+
+
+  validates :name, length: { minimum: 2, maximum: 20 }, uniqueness: true
+  validates :introduction, length: { maximum: 50 }
+
 end
