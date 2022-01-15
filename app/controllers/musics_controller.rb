eb8eca3b6ad9a581705d3e8a7452970ec246705d
EC2 Default User <usuikenta100@gmail.com>
2022-01-15 09:04:10 +0000

    [fix]

--- app/controllers/musics_controller.rb
+++ app/controllers/musics_controller.rb
@@ -1,6 +1,7 @@
 class MusicsController < ApplicationController
     def groups
         # groupテーブルかcategoryテーブルのどっちかにFK(〇〇_idカラム)を追加して、リレーションする
-        # @groups = Group.where(Group.category.name = "音楽")
+        # @groups = Group.where(Group.category.name = "音楽") → できないかも
+        # @groups = Music.groups
     end
 end
